78eb34ed5a53cc188413a554a801b6c980cc102c
websitelink12 <mohammaddafa246@gmail.com>
2025-03-16 23:55:52 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const audio = document.getElementById("audio");
+const playButton = document.getElementById("play");
+
+playButton.addEventListener("click", () => {
+    if (audio.paused) {
+        audio.play();
+        playButton.textContent = "⏸";
+    } else {
+        audio.pause();
+        playButton.textContent = "▶";
+    }
+});
